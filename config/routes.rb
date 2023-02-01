b5fdeac634a216e42d9dfd22f941d3afcc775e8b
dhealy83 <drtjprdave@gmail.com>
2023-02-01 12:43:49 -0700

    Created roles: user, admin, member, moderator.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'pages/home'
   get 'pages/members'
   get 'pages/leagues'
   get 'pages/tournaments'
   get 'pages/bag_tags'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   root 'pages#home'
+
+  devise_for :users
 end
